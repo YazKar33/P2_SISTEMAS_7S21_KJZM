3c4f1f70ece77ba0adfcf6c10a937ee1fcf10c6b
Karla Jazmin Zuñiga Moreno <karly33zm@gmail.com>
2023-09-26 03:55:17 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/p2_sistemas_7s21_kjzm/SegundaActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.isc.tesoem.p2_sistemas_7s21_kjzm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
